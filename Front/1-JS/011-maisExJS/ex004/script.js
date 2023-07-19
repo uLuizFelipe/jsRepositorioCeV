724f2f5e4d12c023d4a492fcefc1fb28810dedd0
luiz felipe pires carvalho <138532255+uLuizFelipe@users.noreply.github.com>
2023-07-19 18:35:36 -0300

    aula14, tava perdido

--- Front/1-JS/011-maisExJS/ex004/script.js
+++ Front/1-JS/011-maisExJS/ex004/script.js
@@ -2,12 +2,42 @@
 
 
 function calcular() {
-    var a = Number(window.document.querySelector(`input#numEntrada`).value)
-        if (a.length == 0) {
+    var numeroForm = Number(window.document.querySelector(`input#numEntrada`).value)
+    var linhaSoma = window.document.querySelector(`select#soma`)
+    var linhaSub = window.document.querySelector(`select#subt`)
+    var linhaMult = window.document.querySelector(`select#mult`)
+    var linhaDiv = window.document.querySelector(`select#divi`)
+        if (numeroForm.length == 0) {
             Window.alert(`Entrada não aceita, insira um valor valido!`)
         }
-            else {
-                
+            else {      
+                linhaSoma.innerHTML=``
+                linhaSub.innerHTML=``
+                linhaMult.innerHTML=``
+                linhaDiv.innerHTML=``
+                    for(var loopSoma = 1;loopSoma <= 10;loopSoma++) {
+                        var item = document.createElement(`option`)
+                        item.text = `${numeroForm} + ${loopSoma} = ${numeroForm+loopSoma}`
+                        linhaSoma.appendChild(item)
+                    }
+
+                    for(var loopSub = 1;loopSub <= 10;loopSub++) {
+                        var item = document.createElement(`option`)
+                        item.text = `${numeroForm} - ${loopSub} = ${numeroForm-loopSub}`
+                        linhaSub.appendChild(item)
+                    }
+
+                    for(var loopMult = 1;loopMult <= 10;loopMult++) {
+                        var item = document.createElement(`option`)
+                        item.text = `${numeroForm} * ${loopMult} = ${numeroForm*loopMult}`
+                        linhaMult.appendChild(item)
+                    }
+
+                    for(var loopDiv = 1;loopDiv <= 10;loopDiv++) {
+                        var item = document.createElement(`option`)
+                        item.text = `${numeroForm} / ${loopDiv} = ${numeroForm/loopDiv}`
+                        linhaDiv.appendChild(item)
+                    }
             }
     
 }
